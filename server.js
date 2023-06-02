f2082641675ca26b59e73f6fa34a99f1d47020dc
unknown <makemoney76311@gmail.com>
2023-06-02 15:46:28 +0100

    F

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 app.use(cors());
+
+const db = mysql.createConnection({
+  database: "bys14cmytvyjy0gfobdf",
+  user: "u0scs1olouwxhfne",
+  host: "bys14cmytvyjy0gfobdf-mysql.services.clever-cloud.com",
+  password: "rVts5mKeHG4AodywiDb2",
+});
 
 app.get("/test", (req, res) => {
   res.json("Done");
 });
 
 app.get("/", (req, res) => {
-  res.json("GET DONE");
+  const sql = "SELECT * FROM users;";
+  db.query(sql, (err, data) => {
+    res.json(data);
+  });
 });
 
 app.post("/", (req, res) => {
