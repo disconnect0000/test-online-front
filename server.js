884c18e3c226943fdeda1609b869287acf9b943c
unknown <makemoney76311@gmail.com>
2023-06-02 15:16:12 +0100

    F

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.json("Done");
+});
+
+app.get("/", (req, res) => {
+  res.json("GET DONE");
+});
+
+app.post("/", (req, res) => {
+  res.json("POST DONE");
 });
 
 app.listen(process.env.PORT || 5000);
